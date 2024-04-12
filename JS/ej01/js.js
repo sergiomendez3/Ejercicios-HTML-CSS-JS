1bfcb9b25f3077f00f6afb982732215f53c1e958
Sergio Méndez Losada <sergio.mendez@campusdual.com>
2024-04-12 15:13:24 +0200

    Ejercicios JS

---
+++ JS/ej01/js.js
@@ -0,0 +1,6 @@
+//Selecciono el div con con id="contenido"
+//Puedo usar variables como "let" o "const"
+const contenidoDiv = document.getElementById("contenido");
+
+//Modifico el contenido del div
+contenidoDiv.textContent = "Hola mundo";
